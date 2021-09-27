cbf8058002d522af0e319b002e982361614fa68c
ankitaj24 <86417055+ankitaj24@users.noreply.github.com>
2021-09-27 15:55:36 +0530

    Add files via upload

---
+++ init.js
@@ -0,0 +1,12 @@
+(function (){
+    function fnAddButtons() {
+        var img=document.createElement("img");
+        img.src="https://img.icons8.com/ios-filled/250/000000/javascript.png";
+        if((document.querySelector("[jscontroller=Wo3n8]"))!=null)
+        document.querySelector("[jscontroller=Wo3n8]").appendChild(img);
+        else
+        document.querySelector("ol.pdswFd").appendChild(img);
+        
+    }
+    fnAddButtons();
+})();
